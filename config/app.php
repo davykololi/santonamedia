cc9f80b7774db8e0d3ad31fb341285032ba40c9a
kololidavid <kololimdavid@gmail.com>
2020-04-28 13:11:19 -0700

    Made changes to config app file

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'newspipemedia'),
+    'name' => env('APP_NAME', 'santonamedia'),
 
     /*
     |--------------------------------------------------------------------------
