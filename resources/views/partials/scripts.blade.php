7c3c4a545155bd52e9518439a73286b9045a31ed
kololidavid <kololimdavid@gmail.com>
2020-05-03 01:05:34 -0700

    Added cruchfy share buttons

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -40,3 +40,39 @@ $.ajaxSetup({headers:{'csrftoken':'{{csrf_token()}}'}});
 
 <!--Twitter share button script-->
 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+
+
+========> Twitter script: <========
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+ 
+ 
+========> Facebook HTML5 script: <=========
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+ 
+ 
+========> Buffer script: <========
+<script type="text/javascript" src="https://d389zggrogs7qo.cloudfront.net/js/button.js"></script>
+ 
+ 
+========> Google+ script: <========
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+ 
+ 
+========> Pinterest script: <========
+<script async defer src="//assets.pinterest.com/js/pinit.js"></script>
+ 
+========> LinkedIn script: <========
+<script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
--- resources/views/partials/sharea.blade.php
+++ resources/views/partials/sharea.blade.php
@@ -48,3 +48,47 @@
 <!-- twitter --button-->
 <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Yes" data-via="santonamedia" data-hashtags="santonamedia" data-related="santonamedia,santonamedia" data-show-count="false">Tweet
 </a>
+
+
+function crunchify_social_sharing_buttons($content) {
+	global $post;
+	if(is_singular() || is_home()){
+	
+		// Get current page URL 
+		$crunchifyURL = urlencode(get_permalink());
+ 
+		// Get current page title
+		$crunchifyTitle = htmlspecialchars(urlencode(html_entity_decode(get_the_title(), ENT_COMPAT, 'UTF-8')), ENT_COMPAT, 'UTF-8');
+		// $crunchifyTitle = str_replace( ' ', '%20', get_the_title());
+		
+		// Get Post Thumbnail for pinterest
+		$crunchifyThumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+ 
+		// Construct sharing URL without using any script
+		$twitterURL = 'https://twitter.com/intent/tweet?text='.$crunchifyTitle.'&amp;url='.$crunchifyURL.'&amp;via=Crunchify';
+		$facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$crunchifyURL;
+		$googleURL = 'https://plus.google.com/share?url='.$crunchifyURL;
+		$bufferURL = 'https://bufferapp.com/add?url='.$crunchifyURL.'&amp;text='.$crunchifyTitle;
+		$linkedInURL = 'https://www.linkedin.com/shareArticle?mini=true&url='.$crunchifyURL.'&amp;title='.$crunchifyTitle;
+ 
+		// Based on popular demand added Pinterest too
+		$pinterestURL = 'https://pinterest.com/pin/create/button/?url='.$crunchifyURL.'&amp;media='.$crunchifyThumbnail[0].'&amp;description='.$crunchifyTitle;
+ 
+		// Add sharing button at the end of page/page content
+		$content .= '<!-- Implement your own superfast social sharing buttons without any JavaScript loading. No plugin required. Detailed steps here: https://crunchify.com/?p=7526 -->';
+		$content .= '<div class="crunchify-social">';
+		$content .= '<h5>SHARE ON</h5> <a class="crunchify-link crunchify-twitter" href="'. $twitterURL .'" target="_blank">Twitter</a>';
+		$content .= '<a class="crunchify-link crunchify-facebook" href="'.$facebookURL.'" target="_blank">Facebook</a>';
+		$content .= '<a class="crunchify-link crunchify-googleplus" href="'.$googleURL.'" target="_blank">Google+</a>';
+		$content .= '<a class="crunchify-link crunchify-buffer" href="'.$bufferURL.'" target="_blank">Buffer</a>';
+		$content .= '<a class="crunchify-link crunchify-linkedin" href="'.$linkedInURL.'" target="_blank">LinkedIn</a>';
+		$content .= '<a class="crunchify-link crunchify-pinterest" href="'.$pinterestURL.'" data-pin-custom="true" target="_blank">Pin It</a>';
+		$content .= '</div>';
+		
+		return $content;
+	}else{
+		// if not a post/page then don't include sharing button
+		return $content;
+	}
+};
+add_filter( 'the_content', 'crunchify_social_sharing_buttons');
