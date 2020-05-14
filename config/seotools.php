310ac624c79b13ec91ed47637a26c294e7351232
kololidavid <kololimdavid@gmail.com>
2020-05-14 02:46:50 -0700

    Configured the seotools file

--- config/seotools.php
+++ config/seotools.php
@@ -57,9 +57,9 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
-            'description' => 'For those who helped create the Genki Dama', // set false to total remove
-            'url'         => false, // Set null for using Url::current(), set false to total remove
+            'title'       => false, // set false to total remove
+            'description' => false, // set false to total remove
+            'url'         => null, // Set null for using Url::current(), set false to total remove
             'type'        => 'WebPage',
             'images'      => [],
         ],
