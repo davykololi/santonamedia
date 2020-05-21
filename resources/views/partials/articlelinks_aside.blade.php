1bb50ef111bfe88bfe132c365076a234614f8e46
kololi david <santonamedia79@gmail.com>
2020-05-21 13:06:39 -0700

    Increased the sidebar-right image percentage

--- resources/views/partials/articlelinks_aside.blade.php
+++ resources/views/partials/articlelinks_aside.blade.php
@@ -3,7 +3,7 @@
 <div class="card">
     <div>
     <a href="{{ route('users.posts.read', ['post_slug' => $archive->slug]) }}">
-        <img class="float-left pull-left mr-2" width="40" src ="/storage/public/storage/{{ $archive->image }}" alt ="{{ $archive->title }}"/> 
+        <img class="float-left pull-left mr-2" width="50" src ="/storage/public/storage/{{ $archive->image }}" alt ="{{ $archive->title }}"/> 
     </a>
 <p>
     <a href="{{ route('users.posts.read', ['post_slug' => $archive->slug]) }}">
