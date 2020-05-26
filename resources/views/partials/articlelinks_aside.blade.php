840dd26b7021dec99e8059b341c8cd63657b432a
kololi david <santonamedia79@gmail.com>
2020-05-26 00:21:14 -0700

    Edited img tags

--- resources/views/partials/articlelinks_aside.blade.php
+++ resources/views/partials/articlelinks_aside.blade.php
@@ -1,7 +1,7 @@
 
 @forelse($archives as $archive)
 <div class="card">
-    <img class="float-left pull-left mr-2" width="50" src="/storage/public/storage/{{ $archive->image }}" alt ="{{ $archive->title }}"/> 
+    <img class="float-left pull-left mr-2" width="50" src = "/storage/public/storage/{{ $archive->image }}" alt ="{{ $archive->title }}"/> 
     <a href="{{ route('users.posts.read', ['post_slug' => $archive->slug]) }}">
         {!! $archive->title !!}
     </a>
